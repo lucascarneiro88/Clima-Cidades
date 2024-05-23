5ac4cbab20e0e909b2bf07368f9696bce99db991
Lucas <carneirolucas88@gmail.com>
2024-05-23 10:14:47 -0300

    chore: adiciona imports e inicializa states em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useState, useEffect} from "react";
+import axios from "axios";
 
 
 import Busca from "./components/BuscaComponents/Busca";
 import Clima from "./components/ClimaComponents/Clima";
 import Previsao from "./components/PrevisaoComponents/Previsao";
 
+
+
 import {Titulo} from "./AppStyles";
 
+
 function App() {
+
+  const [cidade, setCidade] = useState("");// string vazia para que seja inserido o nome da cidade
+  const [clima, setClima] = useState(null);// array como null para indicar que ainda não há previsões do tempo disponíveis
+  const [previsao, setPrevisao] =useState([]);//array vazio para tipo lista
+
 
 
   return (
