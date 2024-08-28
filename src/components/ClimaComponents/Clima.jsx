46439f6327f19dc9940d302a44e3cac4c227a931
Ana Paula <anapaulaprates8@gmail.com>
2024-08-28 15:39:34 -0300

    feat: Adiciona exibição  de clima

--- src/components/ClimaComponents/Clima.jsx
+++ src/components/ClimaComponents/Clima.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Clima() {
+function Clima({clima}) {
   return (
     <div>
-    <h1>Nome:</h1>
-    <img src="" alt="" />
-    <p>°C</p>
-    <p>Descrição da temperatura:</p>
+    <h3>Cidade: {clima.name}</h3>
+    <img src={`http://openweathermap.org/img/wn/${clima.weather[0].icon}.png`} alt={clima.weather[0].description} />
+    <p>Temperatura: {clima.main.temp}°C</p>
+    <p>Descrição da temperatura: {clima.weather[0].description}</p>
     </div>
   )
 }
