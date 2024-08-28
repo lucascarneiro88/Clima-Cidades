33eb1c46dbe75d834f9d31aa4ae14a6b2469ba29
Ana Paula <anapaulaprates8@gmail.com>
2024-08-28 15:36:15 -0300

    feat: Adiciona funcionalidade de busca de clima

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,47 @@
-import { useState, useEffect} from "react";
+import { useState } from "react";
 import axios from "axios";
-
 
 import Busca from "./components/BuscaComponents/Busca";
 import Clima from "./components/ClimaComponents/Clima";
 import Previsao from "./components/PrevisaoComponents/Previsao";
 
-
-
-import {Titulo} from "./AppStyles";
-
+import { Titulo } from "./AppStyles";
 
 function App() {
+  const [cidade, setCidade] = useState(""); // string vazia para que seja inserido o nome da cidade
+  const [clima, setClima] = useState(null); // array como null para indicar que ainda não há previsões do tempo disponíveis
+  const [previsao, setPrevisao] = useState([]); // array vazio para tipo lista
 
-  const [cidade, setCidade] = useState("");// string vazia para que seja inserido o nome da cidade
-  const [clima, setClima] = useState(null);// array como null para indicar que ainda não há previsões do tempo disponíveis
-  const [previsao, setPrevisao] =useState([]);//array vazio para tipo lista
+  const apiKey = import.meta.env.VITE_API_KEY || ""; // VARIAVEL PARA BUSCAR DADOS
 
-
+  // Função para buscar o clima
+  const buscarClima = async () => {
+    try {
+      const respostaClima = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`
+      );
+      const respostaPrevisao = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`
+      );
+      setClima(respostaClima.data);
+      setPrevisao(respostaPrevisao.data.list.slice(0, 5));
+    } catch (error) {
+      if (error.response) {
+        console.log("Erro na API:", error.response.status, error.response.data);
+      } else {
+        console.log("Erro na requisição:", error.message);
+      }
+    }
+  };
+  console.log(clima);
+  console.log(previsao);
 
   return (
     <div>
       <Titulo>Condições Climáticas</Titulo>
-      <Busca />
-      <Clima />
-      <Previsao />
-
+      <Busca cidade={cidade} setCidade={setCidade} buscarClima={buscarClima} />
+      {clima && <Clima clima={clima} />}
+      {previsao.length > 0 && <Previsao previsoes={previsao} />}
     </div>
   );
 }
