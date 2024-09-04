30307907e126a2f5a4f23c40d0f9e95edd6b1010
Lucas <carneirolucas88@gmail.com>
2024-09-04 11:05:01 -0300

    style: criando estilos para componente previsao

---
+++ src/components/PrevisaoComponents/PrevisaoStyles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const PrevisaoContainer = styled.div `
+margin-top: 20px;
+padding: 15px;
+border-radius: 5px;
+box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+
+h4 {
+    text-align: center;
+    margin-bottom: 1px;
+    color: #333;
+    /* font-size: 22px; */
+}
+
+ul {
+    list-style: none;//tira o marcadeo da lista não ordenada 
+    padding: 0;
+}
+
+li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    margin-bottom: 5px;
+
+    img {
+        margin-right:10px;
+    }
+
+}
+
+
+`;
