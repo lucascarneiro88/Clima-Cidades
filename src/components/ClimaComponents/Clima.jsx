06e8e849ccb99e863dff9cc39b0e4efa1e02348c
Lucas <carneirolucas88@gmail.com>
2024-09-04 11:03:28 -0300

    feat: removendo descrição como titulo de informações de clima

--- src/components/ClimaComponents/Clima.jsx
+++ src/components/ClimaComponents/Clima.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 
 import "./CimaStyles";
+import { ClimaInformacao } from './CimaStyles';
 
 function Clima({clima}) {
   return (
-    <div>
-    <h3>Cidade: {clima.name}</h3>
+    <ClimaInformacao>
+    <h3>{clima.name}</h3>
     <img src={`http://openweathermap.org/img/wn/${clima.weather[0].icon}.png`} alt={clima.weather[0].description} />
-    <p>Temperatura: {clima.main.temp}°C</p>
-    <p>Descrição da temperatura: {clima.weather[0].description}</p>
-    </div>
+    <p> {clima.main.temp}°C</p>
+    <p>{clima.weather[0].description}</p>
+    </ClimaInformacao>
   )
 }
 
