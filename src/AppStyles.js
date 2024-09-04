54df83863abbbec440164ba8bb50c36511190227
Lucas <carneirolucas88@gmail.com>
2024-09-04 11:00:37 -0300

    style: criando estilo para componente app , pagina principal

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -4,3 +4,14 @@ export const Titulo = styled.h2`
   font-size: 2rem;
   margin-bottom: 20px;
 `;
+
+export const ClimaContainer = styled.div`
+background: linear-gradient(to bottom, #89cff0, #005c99);
+
+color: #FFF;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+height: 100vh;
+`;
