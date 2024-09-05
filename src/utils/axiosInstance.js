42bf93dcd5102f5f5a7e718cee0b5f6faee1cfb1
Lucas <carneirolucas88@gmail.com>
2024-09-05 14:04:20 -0300

    feat: configuando axiosInstance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 //criando a instancia do Axios
-
-const axiosInstance =axios.create({
-    baseURL: "https://api.openweathermap.org/data/2.5/", // URL base da API
-    params: {
-        appid:import.meta.env.VITE_API_KEY, // A chave da API, vinda das variáveis de ambiente
-        units: "metric",// Unidades de medida da Api
-        lang: "pt_br", // Idioma
-    },
+const axiosInstance = axios.create({
+  baseURL: "https://api.openweathermap.org/data/2.5/", // URL base da API
+  params: {
+    appid: import.meta.env.VITE_API_KEY, // A chave da API, vinda das variáveis de ambiente
+    units: "metric", // Unidades de medida da Api
+    lang: "pt_br", // Idioma
+  },
 });
 
 export default axiosInstance;
