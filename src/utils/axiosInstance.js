113526419f8034ab1ec848e5428fea5619e94da6
Lucas <carneirolucas88@gmail.com>
2024-09-05 13:28:56 -0300

    feat: configuração axios instance para Api request

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,4 +2,13 @@ import axios from "axios";
 
 //criando a instancia do Axios
 
-const axiosInstance
+const axiosInstance =axios.create({
+    baseURL: "https://api.openweathermap.org/data/2.5/", // URL base da API
+    params: {
+        appid:import.meta.env.VITE_API_KEY, // A chave da API, vinda das variáveis de ambiente
+        units: "metric",// Unidades de medida da Api
+        lang: "pt_br", // Idioma
+    },
+});
+
+export default axiosInstance;
